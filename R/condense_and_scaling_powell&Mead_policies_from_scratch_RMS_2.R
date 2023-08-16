98d25cc85c1bb4d386e4f514fd553ce13c4541c7
ElleStark <elle.stark5645@gmail.com>
2023-08-16 16:16:23 -0600

    Updated scripts for new dvs

--- R/condense_and_scaling_powell&Mead_policies_from_scratch_RMS_2.R
+++ R/condense_and_scaling_powell&Mead_policies_from_scratch_RMS_2.R
@@ -1,12 +1,7 @@
-
+# Process policy set data and create barplots representing decision variables
 # Script modified from version provided by Reclamation in June 2022
 # Modified by E Stark August 2023
 
-
-#setwd("C:\\Users\\ealexander\\Documents\\MORDM Continuation\\Internal Work\\MOEA\\Mead-and-Powell\\NewMaster_10-08-21")
-
-#setwd("Z:/Shared/Research/DMDU continuation/internal work/Mead-and-Powell/RampingRun_03-2-22_10kFE")
-setwd("D:/PowellPols_data/FullRampingRun_6.6.22")
 
 rm(list=ls())
 
@@ -23,14 +18,14 @@ library(plotrix)
 #library(tidyverse)
 library(readxl)
 
-workbook.name = "Archive_FullHydrologyRampingRun_5kFE_withID.xlsx"
+workbook.name = "data/inputs/Archive_4Selected.xlsx"
 #wb = loadWorkbook(workbook.name)
 wb = read_excel(workbook.name)
 #df = readWorksheet(wb, sheet = "Sheet1", header = TRUE)
-df = wb[1:284,]
+df = wb[1:6,]
 
 #for testing, create snapshot of original df
-#df_og = df
+df_og = df
 
 
 ########################################
@@ -41,16 +36,15 @@ df = wb[1:284,]
 
 ###create a powell df (PTiering) for each policy
 for (j in 1:nrow(df)){
-  #j=1
-  PTierEl = t(df[j, 20:24])
-  MeadRefs = t(df[j,25:29])
-  RelRanges = t(df[j,30:34])
-  BalVars = t(df[j,35:39])
-  PRels = t(df[j,40:44])
+  PTierEl = t(df[j, 19:23])
+  MeadRefs = t(df[j, 29:33])
+  BalMaxOffset = t(df[j, 34:38])
+  BalMinOffset = t(df[j, 39:43])
+  PRels = t(df[j, 24:28])
   
-  PTiering = as.data.frame(cbind(PTierEl, MeadRefs, RelRanges, BalVars, PRels))
+  PTiering = as.data.frame(cbind(PTierEl, MeadRefs, BalMaxOffset, BalMinOffset, PRels))
   row.names(PTiering) = c("PT1", "PT2", "PT3", "PT4", "PT5")
-  colnames(PTiering) = c("PTierEl", "MeadRefEl", "RelRanges", "BalVars", "PRels")
+  colnames(PTiering) = c("PTierEl", "MeadRefEl", "BalMaxOffset", "BalMinOffset", "PRels")
   
   # create examples to test functionality
   
@@ -62,7 +56,7 @@ for (j in 1:nrow(df)){
   
   #for testing, create snapshot of PTiering before any translations
   
-  #PTieringOG = PTiering
+  PTieringOG = PTiering
   
   #replace vals in cols 2-5 w/ row that has highest value of primary release (col 5)
   
@@ -91,18 +85,18 @@ for (j in 1:nrow(df)){
   if (PTiering[nrow(PTiering),1]==3370 && PTiering[1,1]!=3370){
     next_min = which(PTiering[,1]==min(PTiering$PTierEl[PTiering$PTierEl > 3370]))
     PTiering[next_min,"MeadRefEl"] = PTiering[next_min,"PRels"] = 99999999
-    PTiering[next_min,"BalVars"] = 1
+    #PTiering[next_min,"BalVars"] = 1
   } 
   
   
   #make MeadRefEl and RElRange = 99999999 if BalVar = 0
   
-  for (i in 1:(nrow(PTiering))){
-    #i=1
-    if (PTiering[i,4] == 0){
-      PTiering[i,2] = PTiering[i,3] = 99999999
-      }
-  }
+  # for (i in 1:(nrow(PTiering))){
+  #   #i=1
+  #   if (PTiering[i,4] == 0){
+  #     PTiering[i,2] = PTiering[i,3] = 99999999
+  #     }
+  #}
   
   
   
@@ -128,32 +122,33 @@ for (j in 1:nrow(df)){
   
   #sort based on col 1
   
-  PTiering = PTiering[order(-PTiering$PTierEl), ] 
+  PTiering = PTiering[order(PTiering$PTierEl, decreasing = TRUE), ] 
   
   
   
   #distribute columns of powell df back into a single row and combine w/ un-condensed mead variables & objective values
 
-  df[j,20:24] = t(PTiering$PTierEl)
-  df[j,25:29] = t(PTiering$MeadRefEl)
-  df[j,30:34] = t(PTiering$RelRanges)
-  df[j,35:39] = t(PTiering$BalVars)
-  df[j,40:44] = t(PTiering$PRels)
+  df[j,19:23] = t(PTiering$PTierEl)
+  df[j,29:33] = t(PTiering$MeadRefEl)
+  df[j,34:38] = t(PTiering$BalMaxOffset)
+  df[j,39:43] = t(PTiering$BalMinOffset)
+  df[j,24:28] = t(PTiering$PRels)
   
   #make EqInc = 0 if PT1e = 3700
-  if(df[j,20]==3700){
-    df[j,45] = 99999999
-  }
+#   if(df[j,20]==3700){
+#     df[j,45] = 99999999
+#   }
 }
 
 
 
 ############################## MEAD ####################
 
-short_elev = t(df[,4:11]) 
-row.names(short_elev) = c("T1e", "T2e", "T3e", "T4e", "T5e", "T6e", "T7e", "T8e")
+short_elev = t(df[,3:10]) 
+rownames(short_elev) = c("T1e", "T2e", "T3e", "T4e", "T5e", "T6e", "T7e", "T8e")
 
-short_vol = df[,12:19]/1000 
+df[,11:18] = apply(df[,11:18], 2, function(x) as.numeric(x))
+short_vol = df[,11:18]/1000 
 short_vol = apply(short_vol, 1, rev) #reverse order of volumes
 row.names(short_vol) = c("T1V", "T2V", "T3V", "T4V", "T5V", "T6V","T7V", "T8V")
 
@@ -219,7 +214,7 @@ for(i in 1:ncol(compressed_vol)){
 
 ### need to sort based on elevation only ###
 #### might need to combine the data frame, then sort everything based on elevaiton ####
-
+compressed_elev = apply(compressed_elev, 2, as.numeric)
 condensed_elev = apply(compressed_elev, 2, sort, decreasing=T)
 condensed_vol = apply(compressed_vol, 2, sort, decreasing=F)
 
@@ -239,49 +234,49 @@ condensed_policies = t(condensed_policies)
 #calculate elevations for two surplus tiers **if domestic surplus distance (d2) = 0, partial surplus goes away
 #and surplus takes on the size defined by d1
 
-surplus_sizes = df[,2:3]#check column indices ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+surplus_sizes = df[,2]#check column indices ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 #surplus_sizes2 = surplus_sizes #for comparison
 
-for(i in 1:nrow(surplus_sizes)){
-  if (surplus_sizes[i,2] == 0){
-    surplus_sizes[i,1:2] = surplus_sizes[i,2:1]
-  }
-}
-
-#also zero-out partial if d1=d2
-for(i in 1:nrow(surplus_sizes)){
-  if (surplus_sizes[i,2] == surplus_sizes[i,1]){
-    surplus_sizes[i,1] = 0
-  }
-}
+# for(i in 1:nrow(surplus_sizes)){
+#   if (surplus_sizes[i,2] == 0){
+#     surplus_sizes[i,1:2] = surplus_sizes[i,2:1]
+#   }
+# }
+# 
+# #also zero-out partial if d1=d2
+# for(i in 1:nrow(surplus_sizes)){
+#   if (surplus_sizes[i,2] == surplus_sizes[i,1]){
+#     surplus_sizes[i,1] = 0
+#   }
+# }
 
 #surplus_sizes2[48,]
 #surplus_sizes[48,] 
 
 
 
-part_surplus_elev = 1200 - surplus_sizes$d1
-surplus_elev = 1200 - surplus_sizes$d2
+#part_surplus_elev = 1200 - surplus_sizes$d1
+surplus_elev = 1200 - as.numeric(surplus_sizes$Mead_Surplus_DV)
 
 #if partial surplus or surplus went away, replace 1200s w/ 99999999
 surplus_elev[surplus_elev==1200] = 99999999
-part_surplus_elev[part_surplus_elev==1200] = 99999999
+#part_surplus_elev[part_surplus_elev==1200] = 99999999
 
 # ID=1:nrow(condensed_policies)
 
-condensed_policies_full = cbind(surplus_elev, part_surplus_elev, condensed_policies)#, df[,45:53]) #check column indices 
+condensed_policies_full = cbind(surplus_elev, condensed_policies)#, df[,45:53]) #check column indices 
 
 #replace Mead variables in original df w/ condensed variables
 
-df = cbind(condensed_policies_full, df[,20:53])
+df = cbind(condensed_policies_full, df[,19:47])
 
 
 ### I didn't see any policies with 8 shortage tiers... need to look into this.... ###
 
 #### write the condensed dataframe to a csv file ####
 
-write.csv(df, "All_values_RampingRun_5kFE_withID_after_condensing.csv")
+write.csv(df, "data/outputs/condensed_DVs_6Selected.csv")
 
 ### Scaling script #################################################################################################
 
@@ -293,7 +288,7 @@ write.csv(df, "All_values_RampingRun_5kFE_withID_after_condensing.csv")
 #df = readWorksheet(wb, sheet = "Sheet1", header = TRUE)
 
  
-numobj = 8
+numobj = 4
 
 archive.mat = as.matrix(df[,(ncol(df)-(numobj-1)):ncol(df)])
 
--- R/create data frames and export stacked bar plots.Powell and Mead.R
+++ R/create data frames and export stacked bar plots.Powell and Mead.R
@@ -1,7 +1,5 @@
 # Prepare data frames for stacked bar plotting of policy decision variables
-# Phase III robustness calculations
-# Nathan Bonham
-# 2/10/21
+# Original: Nathan Bonham 2/10/21, Modified by E Stark Aug 2023
 
 library(tidyr)
 library(dplyr)
@@ -13,9 +11,7 @@ library(patchwork)
 
 rm(list=ls())
 
-#setwd("Z:/Shared/Research/DMDU continuation/Kasprzyk project 2019/files from Nathan/R code/plotting related")
-setwd("D:/PowellPols_data/FullRampingRun_6.6.22")
-Archive='FullHydrology_RampingRun_5kFE_withID_after_condensing.csv'
+Archive='data/outputs/condensed_DVs_6Selected.csv'
 
 Archive.df=read.table(Archive, header = T, sep = ",")
 
@@ -32,28 +28,28 @@ lastDV=which(colnames(Archive.df)=='T8V')
 dv=Archive.df[,2:lastDV]
 
 # prepare for plotting
-elevation=dv[1:10]
+elevation=dv[1:9]
 elevation=cbind(rep(1220,nrow(elevation)), elevation)
-names(elevation) = c("Top",names(elevation[2:11]))
+names(elevation) = c("Top",names(elevation[2:10]))
 elevation$dead.pool=895 # need to add dead pool or tier 6 is thrown out for policies where it exists
 
 #when one or more surplus tiers does not exist, need to replace number to make e_delta work
 for(j in 1:nrow(elevation)){
   if (elevation[j,2]==99999999)
     elevation[j,2] = 1220
-  if (elevation[j,3]==99999999)
-    elevation[j,3] = elevation[j,2]
+  # if (elevation[j,3]==99999999)
+  #   elevation[j,3] = elevation[j,2]
 }
 
-el_max=unlist(apply(elevation, 1, max)) # save max elevation for ggplotting later
+el_max=unlist(apply(elevation[2:10], 1, max)) # save max elevation for ggplotting later
 
-e_delta = elevation[,1:11] #initialize e_delta
+e_delta = elevation[,1:10] #initialize e_delta
 
-for(i in 2:12){
+for(i in 2:11){
   e_delta[i-1]=elevation[i-1]-elevation[i] # difference between tiers, used for plotting
 }
 
-volume=dv[11:18]
+volume=dv[10:17]
 volume[volume==99999999] =0
 volume$dead.pool=0
 maxVol=apply(volume,1, max)
@@ -69,10 +65,10 @@ volume_labs[volume_labs=="0 KAF"]=NA
 
 volume_labs=data.frame(volume_labs) 
 #add 3 columns to volume df so it becomes the same number of columns as e_delta (which has top and surplus tiers now)
-volume = cbind(rep(NA,nrow(volume)), rep(NA,nrow(volume)),rep(NA,nrow(volume)), volume)
-names(volume) = c("Top","Surplus", "PartSurplus", names(volume[4:12]))
+volume = cbind(rep(NA,nrow(volume)), rep(NA,nrow(volume)), volume)
+names(volume) = c("Top","Surplus", names(volume[3:11]))
 
-volume_labs = cbind(rep("Surplus",nrow(volume_labs)), rep("Partial Surplus",nrow(volume_labs)), rep("Normal",nrow(volume_labs)), volume_labs)
+volume_labs = cbind(rep("Surplus",nrow(volume_labs)), rep("Normal",nrow(volume_labs)), volume_labs)
 names(volume_labs) = names(volume)
 
 # add policy ID to each data frame
@@ -88,7 +84,7 @@ piv_col=which(colnames(e_delta)=='dead_pool')
 elevation=pivot_longer(elevation, cols=1:piv_col, names_to = 'Tier')
 e_delta=pivot_longer(e_delta, cols=1:piv_col, names_to = 'Tier') 
 
-e_delta$Tier=factor(e_delta$Tier, levels = c("Top","surplus_elev", "part_surplus_elev", "T1e","T2e","T3e","T4e","T5e", "T6e", "T7e", "T8e", "dead_pool")) # need to reorder the factor levels such that dead pool is last
+e_delta$Tier=factor(e_delta$Tier, levels = c("Top","surplus_elev", "T1e","T2e","T3e","T4e","T5e", "T6e", "T7e", "T8e", "dead_pool")) # need to reorder the factor levels such that dead pool is last
 e_delta=dplyr::rename(e_delta, 'delta'='value')
 # stacked bar plot order depends on the level order
 
@@ -98,11 +94,11 @@ volume_labs=pivot_longer(volume_labs, cols=1:piv_col, names_to = 'Tier')
 volume_labs=data.frame(volume_labs, elevation=elevation$value)
 
 #remove label for partial surplus when it doesn't really exist (can start at row 2 b/c row 1 is Surplus)
-for (i in 2:nrow(volume_labs)){
-  if(volume_labs[i,2]=="PartSurplus" && volume_labs[i,4]==volume_labs[i-1,4]){
-    volume_labs[i-1,3] = NA
-  }
-}
+# for (i in 2:nrow(volume_labs)){
+#   if(volume_labs[i,2]=="PartSurplus" && volume_labs[i,4]==volume_labs[i-1,4]){
+#     volume_labs[i-1,3] = NA
+#   }
+# }
 
 #remove label for surplus when it doesn't really exist (can start at row 2 b/c row 1 is Top)
 for (i in 2:nrow(volume_labs)){
@@ -112,22 +108,24 @@ for (i in 2:nrow(volume_labs)){
 }
 
 
-
 #add color to volume df
-volume_col = as.data.frame(read.table("vol_gradient3.txt", header = T))
+volume_col = as.data.frame(read.table("data/inputs/vol_gradient3.txt", header = T))
 volume_col$color = as.character(volume_col$color)
+# added bigger volumes to txt file: 2600, 3000, 4000 taf
+
 
 for(i in 1:nrow(volume_col)){
 volume_col[i,2] = paste(as.character("#"), volume_col[i,2], sep = "")
 }
 
 volume$color=""
+volume$value = as.numeric(volume$value)
 
 for(i in 1:nrow(volume)){
-  if(is.na(volume[i,3]) || volume[i,3]==0){
+  if(is.na(volume[i,3]) || as.numeric(volume[i,3])==0){
     volume[i,4] = "#000000"
   }
-  else if(volume[i,3]>0){
+  else if(as.numeric(volume[i,3])>0){
     p = as.numeric(volume[i,3])
     volume[i,4] = volume_col[which(volume_col$volume==p),2]
   }
@@ -147,8 +145,8 @@ for(i in 1:nrow(volume)){
 }
 
 df=data.frame(e_delta, v_lab= volume_labs$value, v_col= volume$color, elevation=volume_labs$elevation, volume=volume$value)
-df$Tier=rep(c('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l'), length(unique(df$policy)))
-df$TierName=rep(c('Surplus', 'Partial Surplus', 'Normal', '1', '2', '3', '4', '5', '6', '7', '8', 'Dead Pool'), length(unique(df$policy)))
+df$Tier=rep(c('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k'), length(unique(df$policy)))
+df$TierName=rep(c('Surplus', 'Normal', '1', '2', '3', '4', '5', '6', '7', '8', 'Dead Pool'), length(unique(df$policy)))
 
 #change to number and add zeros for image ordering in tableau
 df$policy = as.numeric(df$policy)
@@ -157,7 +155,7 @@ df$policy = sprintf("%04d", df$policy)
 ############# Create data frame for policy labels, SOM node labels, and label y position
 
 stacked_bar_plot_data=list(long_data=df)#, wide_data=policy_SOM_labs)
-saveRDS(stacked_bar_plot_data, file='data for stacked mead bar plot.rds')
+saveRDS(stacked_bar_plot_data, file='data/outputs/data for stacked mead bar plot.rds')
 
 write.csv(df, "df.csv")
 
@@ -165,12 +163,12 @@ write.csv(df, "df.csv")
 
 #########################  POWELL  ##################################
 
-firstpDV=which(colnames(Archive.df)=='PT1e')
-lastpDV=which(colnames(Archive.df)=='EqInc')
+firstpDV=which(colnames(Archive.df)=='Powell_Tier_Elevation_DV.Row.0')
+lastpDV=which(colnames(Archive.df)=='Powell_Balance_Min_Offset_DV.Row.4')
 
 pdv=Archive.df[,firstpDV:lastpDV]
 
-rel_ranges.df=read.table("powell_release_range_table.txt", header = T, sep = )
+#rel_ranges.df=read.table("powell_release_range_table.txt", header = T, sep = )
 
 #################### Create data frame for stacked bar plots ###################
 
@@ -191,41 +189,46 @@ for(i in 2:ncol(p.elevation)){
 }
 
 
-mead_ref = pdv[,6:10]
+mead_ref = pdv[,11:15]
 mead_ref=cbind(rep(NA,nrow(mead_ref)), mead_ref)
 names(mead_ref) = c("Equalization",names(mead_ref[2:ncol(mead_ref)]))
 
-ranges = pdv[11:15]
-ranges=cbind(rep(NA,nrow(ranges)), ranges)
-names(ranges) = c("Equalization",names(ranges[2:ncol(ranges)]))
+minoffsets = pdv[21:25]/1000000 # start with min offset
+minoffsets=cbind(rep(NA,nrow(minoffsets)), minoffsets)
+names(minoffsets) = c("Equalization",names(minoffsets[2:ncol(minoffsets)]))
+maxoffsets = pdv[16:20]/1000000
+maxoffsets=cbind(rep(NA,nrow(maxoffsets)), maxoffsets)
+names(maxoffsets) = c("Equalization",names(maxoffsets[2:ncol(maxoffsets)]))
 
-#translate the range variable to the actual release ranges
-for(i in 1:nrow(ranges)){
-  for(j in 1:ncol(ranges)){
-    if (is.na(ranges[i,j]) == F){
-      trynum = ranges[i,j]
-      ranges[i,j] = paste(rel_ranges.df$Lower_Limit[which(rel_ranges.df$Range_Number==trynum)], 
-                          "-", 
-                          rel_ranges.df$Upper_Limit[which(rel_ranges.df$Range_Number==trynum)], 
-                          "maf",
-                          sep = " ")
-    }
-  }
-}
+# append max offset info:
+#(old:)translate the range variable to the actual release ranges
+# for(i in 1:nrow(ranges)){
+#   for(j in 1:ncol(ranges)){
+#     if (is.na(ranges[i,j]) == F){
+#       minoffset = ranges[i,j]
+#       maxoffset = maxoffsets[i,j]
+#       ranges[i,j] = paste(maxoffset, 
+#                           "-", 
+#                           minoffset, 
+#                           "maf",
+#                           sep = " ")
+#     }
+#   }
+# }
 
-bal_vars = pdv[,16:20]
-bal_vars[bal_vars==0] = "Off"
-bal_vars[bal_vars==1] = "On"
-bal_vars=cbind(rep(NA,nrow(bal_vars)), bal_vars)
-names(bal_vars) = c("Equalization",names(bal_vars[2:ncol(bal_vars)]))
+# bal_vars = pdv[,16:20]
+# bal_vars[bal_vars==0] = "Off"
+# bal_vars[bal_vars==1] = "On"
+# bal_vars=cbind(rep(NA,nrow(bal_vars)), bal_vars)
+# names(bal_vars) = c("Equalization",names(bal_vars[2:ncol(bal_vars)]))
 
-Releases = pdv[,21:25]
+Releases = pdv[,6:10]
 Releases = Releases/1000000 #change to MAF
 Releases=cbind(rep(NA,nrow(Releases)), Releases)
 names(Releases) = c("Equalization",names(Releases[2:ncol(Releases)]))
 
-EqInc = as.data.frame(pdv[,26])
-names(EqInc) = "EqInc"
+#EqInc = as.data.frame(pdv[,26])
+#names(EqInc) = "EqInc"
 
 #create labels
 tier_labs = matrix(NA, nrow=nrow(p.elevation), ncol=6) #hardcoded for now
@@ -235,22 +238,27 @@ tier_labs = as.data.frame(tier_labs)
 #special case for equalization tier
 for(k in 1:nrow(tier_labs)){
   if(p.e_delta[k,1]>0){
-    tier_labs[k,1] = paste("Eq Line increases by", EqInc[k,1], "ft/yr", sep = " ")
+    tier_labs[k,1] = "Equalization"
   }
-  
+  # if(p.e_delta[k,1]>0){
+  #   tier_labs[k,1] = paste("Eq Line increases by", EqInc[k,1], "ft/yr", sep = " ")
+  # }
+
 }
 
 for(i in 1:nrow(tier_labs)){
   for(j in 2:ncol(tier_labs)){
-    if(is.na(bal_vars[i,j])==F && bal_vars[i,j]=="On"){
-      tier_labs[i,j] = paste("Release", Releases[i,j], "maf;", "Balance", ranges[i,j], "if Mead <", mead_ref[i,j], "ft",  sep = " ")
+    if(is.na(mead_ref[i,j])==F){
+      tier_labs[i,j] = paste("Release", Releases[i,j], "maf;", "Balance ", 
+                             Releases[i,j]-minoffsets[i,j], "-", Releases[i,j]+maxoffsets[i,j], 
+                             "maf if Mead <", mead_ref[i,j], "ft",  sep = " ")
     }
-    else if(is.na(bal_vars[i,j])==F && bal_vars[i,j]=="Off"){
-      tier_labs[i,j] = paste("Release", Releases[i,j], "maf;", "No Balancing", sep = " ")
+    else if(is.na(Releases[i,j])==F) {
+      tier_labs[i,j] = paste("Release", Releases[i,j], "maf;")
     }
-    else{#(is.na(bal_vars[i,j])){
-      tier_labs[i,j]=NA
-    }
+    else{
+       tier_labs[i,j]=NA
+     }
   }
 }
 names(tier_labs) = c("Equalization Label", "Tier1 Label", "Tier2 Label", "Tier3 Label", "Tier4 Label", "Tier5 Label" )
@@ -268,17 +276,20 @@ tier_labs$policy=as.character(1:nrow(pdv))
 mead_ref$dead.pool=3370
 mead_ref$policy=as.character(1:nrow(pdv))
 
-ranges$dead.pool="3370"
-ranges$policy=as.character(1:nrow(pdv))
+minoffsets$dead.pool=3370
+minoffsets$policy=as.character(1:nrow(pdv))
 
-bal_vars$dead.pool="3370"
-bal_vars$policy=as.character(1:nrow(pdv))
+maxoffsets$dead.pool=3370
+maxoffsets$policy=as.character(1:nrow(pdv))
+
+# bal_vars$dead.pool="3370"
+# bal_vars$policy=as.character(1:nrow(pdv))
 
 Releases$dead.pool=3370
 Releases$policy=as.character(1:nrow(pdv))
 
 
-EqInc$policy=as.character(1:nrow(pdv))
+#EqInc$policy=as.character(1:nrow(pdv))
 
 
 #pivot longer
@@ -287,8 +298,10 @@ p.elevation=pivot_longer(p.elevation, cols=1:ncol(p.elevation)-1, names_to = 'Ti
 p.e_delta=pivot_longer(p.e_delta, cols=1:ncol(p.e_delta)-1, names_to = 'Tier')
 tier_labs=pivot_longer(tier_labs, cols=1:ncol(tier_labs)-1, names_to = 'Tier')
 mead_ref=pivot_longer(mead_ref, cols=1:ncol(mead_ref)-1, names_to = 'Tier')
-ranges=pivot_longer(ranges, cols=1:ncol(ranges)-1, names_to = 'Tier')
-bal_vars=pivot_longer(bal_vars, cols=1:ncol(bal_vars)-1, names_to = 'Tier')
+minoffsets=pivot_longer(minoffsets, cols=1:ncol(minoffsets)-1, names_to = 'Tier')
+maxoffsets=pivot_longer(maxoffsets, cols = 1:ncol(maxoffsets)-1, names_to = 'Tier')
+#ranges=pivot_longer(ranges, cols=1:ncol(ranges)-1, names_to = 'Tier')
+#bal_vars=pivot_longer(bal_vars, cols=1:ncol(bal_vars)-1, names_to = 'Tier')
 Releases=pivot_longer(Releases, cols=1:ncol(Releases)-1, names_to = 'Tier')
 
 p.df=data.frame(p.elevation, delta=p.e_delta$value, t_lab= tier_labs$value)
@@ -312,7 +325,8 @@ write.csv(p.df, "p.df.csv")
 for (i in 1:nrow(Archive.df)){
 policy_id = sprintf("%04d", i)
 
-powell_pol = ggplot(subset(p.df, policy %in% c(policy_id)), aes(fill=Tier, y=delta, x=policy, label=t_lab))+
+powell_pol = ggplot(subset(p.df, policy %in% c(policy_id)), aes(fill=Tier, y=delta, 
+                                                                x=policy))+ #label=t_lab 
   geom_bar(position="stack", stat="identity", color="black", show.legend = FALSE)+
   scale_fill_manual(values=c(
     "#1BBC9B", 
@@ -322,7 +336,7 @@ powell_pol = ggplot(subset(p.df, policy %in% c(policy_id)), aes(fill=Tier, y=del
     "#79BD8F",
     "#289976",
     "#26A69A"))+
-  geom_text(position = position_stack(vjust = .5), size=2.5)+
+  #geom_text(position = position_stack(vjust = .5), size=2.5)+
   theme_minimal()+
   ggtitle("Lake Powell")+
   theme(plot.title = element_text(hjust = .5))+
@@ -333,10 +347,11 @@ powell_pol = ggplot(subset(p.df, policy %in% c(policy_id)), aes(fill=Tier, y=del
 
 mead_col = as.character(df$v_col[which(df$policy==policy_id)])
 
-mead_pol = ggplot(data=subset(df, policy %in% c(policy_id)), aes(fill=Tier, y=delta, x=policy, label=v_lab))+
+mead_pol = ggplot(data=subset(df, policy %in% c(policy_id)), aes(fill=Tier, y=delta, 
+                                                                 x=policy))+ #label=v_lab 
   geom_bar(position="stack", stat="identity",  color="black",  show.legend = FALSE)+
   scale_fill_manual(values=mead_col)+
-  geom_text(position = position_stack(vjust = .5), size=2.5)+
+  #geom_text(position = position_stack(vjust = .5), size=2.5)+
   theme_minimal()+
   ggtitle("Lake Mead")+
   theme(plot.title = element_text(hjust = .5))+
@@ -348,7 +363,7 @@ powell_pol + mead_pol
 
 both.fig = powell_pol + mead_pol
 
-ggsave(paste("ramping images/policy.", policy_id,".png", sep = ""), 
+ggsave(paste("data/outputs/policy images no lab/policy.", policy_id,".png", sep = ""), 
        plot = both.fig, 
        device = "png",
        width = 1650, height = 1420, units = "px",
